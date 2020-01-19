454a3787569c2367d01a4322f4a9d04ce5104eb1
liumengmei <liumengmei@51bsi.com>
2020-01-19 12:44:09 +0800

    update

---
+++ js/data.js
@@ -0,0 +1,297 @@
+[
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJoHXiawddQecWE3Mn3mFTxicCqcGhTAWa9hJ6Ir8QAdVF2QCQpWTnvTIX3Ot8BKydxeT9c51sczKHA/132",
+    "nick_name": "CHERISH",
+    "mobile": "15997486183"
+  },
+  {
+
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIN9bmhxiaP8A6cadNQ2yuumpsoZjzXKkQicnze7KiaiahOhSDRjYqjm4Km5zdMnBmzjicsicQD10vFZDZw/132",
+    "nick_name": "大奇",
+    "mobile": "15927032038"
+  }, {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKicSTCoh2BTngfgkUN9PzjbtrmnWBL2BFPKiaApFAPjVEUCiaW2QVZKD2bIFTiat6HeG5Gehq9tHAiapA/132",
+    "nick_name": "Monsters_xx",
+    "mobile": "18062222069"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/dJxv0FLOzwoGQiarv4BNTon3jm3d9fTakEyvKn9ZAGx2Wdq4nqzSzuicf9IdXfGhjSWiaHMgic8vdazwAyJRbp4V9g/132",
+    "nick_name": "Carina",
+    "mobile": "18772738897"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKic2dZ7ib6p1PMdOYibUhPcYazEgze6nW8gVGxJ4C0o9zvJeR6BgHxosCNW0N2ICv1qqatvkVYEE2DA/132",
+    "nick_name": "啊柒",
+    "mobile": "15927427723"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q7gGQLYiaiaGwMURGwTCV3bibQphAfMol8MP2pAckmhDQNGmeechAfcGz4DabgrZNu8OENp9xzwPy48RPibfWhxrwA/132",
+    "nick_name": "非是藉秋风",
+    "mobile": "15172528573"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/P2wWic1IB46FyQOSI1xmRGXOn9skhYu9EQIMfmEsEPTJccqp13qdN1ctDRu2ibLNAiaHuR8crvAXic9AMp9vibXBMUQ/132",
+    "nick_name": "茈钕籽",
+    "mobile": "15011146916"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWveUcua5t8z9Wz5IOhPicUhDgQp5g8ykwqaWAbAg9Dn2jXMvSqiccia4icztiaOOo9bbZv4LUv4Amjpg/132",
+    "nick_name": "Levana",
+    "mobile": "18202740074"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKib60RibEwtlCDV4SfoY7pOiahdq7K2t7BhhgyBGw0SZAxcmNvf4IGsibnia34RClEEfypibodl6VrJG2g/132",
+    "nick_name": "大雄的切糕梦",
+    "mobile": "13227665777"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoJ8fUFEicTEPJb4Vf9QvVmQhpgibhhCmDhdrdBG4shjk986p8E6QuPicDWb2Yv2bJAeN4nSkaH40Reg/132",
+    "nick_name": "谢邵虎",
+    "mobile": "18986178963"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIM0aLd8cjg3wH2Zk0qqF2JJdJ9ZXmFsGlHia0mA20f4WUVqCPc16JK6k9zsuann21zA4lsgibbjsLA/132",
+    "nick_name": "洛",
+    "mobile": "13164150223"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erx1eGaJeLt1sQy1tM9lOfnJs6YMZGibOtIlJ1wgIfybyCdpjmcRCtJjtE227RDdsTByvUNsob3gGw/132",
+    "nick_name": "Mr.D",
+    "mobile": "18908680082"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/0hupKSIJlkOpSHczCDbm5hpnyaEocB3nLkUT2ATfjqrux7C3sByjicLFXv7sib9cCpQNJ6nHdquibUu6gOaNSFe0w/132",
+    "nick_name": "****",
+    "mobile": "17320555559"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLOKz35nIwYmQBHEfcFEEEX74sLPRyKGcCUNUyAdGxYMOVvYSwp50HRtAQaqhMmO3dqem4XDLJ6dw/132",
+    "nick_name": "shelke",
+    "mobile": "15071141961"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKkPibGWUPadLCdpWRDuBfaYXCj7SDewqmrUdshgpcTMAuseibyhDP72fUEPBusBjpt5B4e7GhHqKcw/132",
+    "nick_name": "sunShineWarn",
+    "mobile": "13659875634"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKKYnTh7pRNyuXPT1g5tylGDwJVvLF3UiahibdtBhf7SKka6jq54GENr2YaqkA4SbQ2aQdtpQfQbzicg/132",
+    "nick_name": "比太阳更耀眼",
+    "mobile": "19945003824"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/zibOvzs9XpVV77nEYnX22KCjjxcic2UbWIicbU3Y02VL8mqQ9ye7QAPic9aDicUbogjYE45S8BMTNrXcMyaTpmOpE4w/132",
+    "nick_name": "🍡🍩🍬",
+    "mobile": "18202797722"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/R8bl29ib9qrnMFADLYBvlVzpDh2bKHicJDJ8vdVRyh2ojJ7LfibW3A2U6Y21y89DXLGHO5PYp5n3icqNm0BtMHOeiaQ/132",
+    "nick_name": "啧啧",
+    "mobile": "15972192581"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/pwLGjziaHbL7Hto1DicZIc1mErHbvubNpfqyrveoUzAYAlFdmEk3nc53rdNxKY23NbdbxEWxf6uiassk5zeGOw1yA/132",
+    "nick_name": "Grace",
+    "mobile": "13971245197"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/qusDOu4ZhMGG96uiamFMnbCHCibqENZ3ovxZvTibzjn9bBx9UYcVa7Iwjza3yXH5PmqZPumRicQwebjY6GNveOodMQ/132",
+    "nick_name": "David",
+    "mobile": "18086681199"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJoHXiawddQecWE3Mn3mFTxicCqcGhTAWa9hJ6Ir8QAdVF2QCQpWTnvTIQrVQIcwWIcj298dl5JQzVA/132",
+    "nick_name": "笑",
+    "mobile": "18696072310"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/LM0PAPLX34y8OfKCPIfqibBaAj4zqSxoMvn7sb0tMKWClEapPAjUpUMNEtNRXf2LmfibjZseLQIc1cdc2iblh67Aw/132",
+    "nick_name": "player",
+    "mobile": "15527128815"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEKiax1piaVZ8jStBjSF5yWfYdcKfOTX3gWGSlicyOzEGTJURbSUibs09d4prxu8tLIFgXTo9NF5qYlugw/132",
+    "nick_name": "二千",
+    "mobile": "19907123964"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/vDXtwQxMDDFuZbhBIXwbDMas1ZUXGuONiayjq2VakkxpwQCI8ZMMKrYxkr2AnEvtiaTWOUuZc4SIHgAfBm8NKTew/132",
+    "nick_name": "哟爱吃小西瓜🍉",
+    "mobile": "18028101695"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eq7BCwXYsI3YS4YHx00NibraRIRsAqiaekSyw3KEEr85GtVRZeibD5ib0BP5xNVibCbDQKUmuN0vd7mAiaQ/132",
+    "nick_name": "zx",
+    "mobile": "18502706605"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/BmUibsiaBYMtFB3xARsHT9PjtQGJBdjfH8ibZVbDMBUbnmZRWo8I7EOYPicA7e2RO2olUkf8iacS0uWZiaZCI5em48Sg/132",
+    "nick_name": "lang",
+    "mobile": "15001726804"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKXKrV4yzdrGACl8iaNgG3MBcF0VwXib2Bib9dFRhHXYfz7Gp2pziaktExghicYvO5BQNJAEvPTVNeZX6w/132",
+    "nick_name": "forrest",
+    "mobile": "13477004093"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epHODNKB6bbQExZrbokbwqGdrIEXoyW6DM72p37VTEyWnVHKiaMu4n2Bzte9rmXAn42n8q4RZYxv0Q/132",
+    "nick_name": "yaner",
+    "mobile": "13277927675"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEKSIdKJCXEfrFoWXXc4zp7lz4jzRV5OeAKcIlniaqB6H9SkVRRk7tXXM8W3Hvj8911bLQXLDaBrwoA/132",
+    "nick_name": "远方的家",
+    "mobile": "13720388543"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/0hupKSIJlkOpSHczCDbm5hpnyaEocB3nLkUT2ATfjqrux7C3sByjicLFXv7sib9cCpnahOAPbhUHkVUtsVToCJ5A/132",
+    "nick_name": "HELLO",
+    "mobile": "17320555559"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIf5CRta3UamjmQM8CYvLblBu2oxjAyjkMkxPy6W1GeVx6O30JG2eqUcvZvRqD5jOPNLyiaTeukViaA/132",
+    "nick_name": "布拉格",
+    "mobile": "18671305063"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/UuIeFacMK2g5vvFxwyNuCV8VyKADvOMUxBnScoX6wEEOLBXwbOvlJlJR1vJkBbx4EPY1iblDQOR7mwlhGPTGezw/132",
+    "nick_name": "安多",
+    "mobile": "13986221679"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/hs9hrPl98IFxbsEZkiaKP6sabBIwhLMxzrCkvwOAF05I4b2JYLPzuVm9ueFyUEpe7eicSpQ0dIEYwHEsWpeZBGxw/132",
+    "nick_name": "嘉哲",
+    "mobile": "13537620582"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/J49mxlu8OibIWCIlmTPlkO5MQo3j10RiczEEDUtE86icot9t1qQ1vyNzInVLIO2qibttvibYTVkLRibMZhqtLD0Q404g/132",
+    "nick_name": "不言而喻",
+    "mobile": "13027137053"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eodOscLlxBFVe7Dhiahz7ErqCn9nNXtbITkVcvAJUg9MicmwBjOVwJPsLnFDPf8zKqnfUl8VUz8F3gg/132",
+    "nick_name": "盒子微信超级会员",
+    "mobile": "18502751130"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJNEmQXXib1rRljeoySuw7CTgRGgAVwuF70LsWYoKZUZP3unLXx1YL37yM5ibeicmyWmJV0pT4elbNdw/132",
+    "nick_name": "大猪蹄子😅",
+    "mobile": "17607199031"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/BgHpE3jibeSeibdBLNJyQYHUib4diasI9AnmJg1pNBtRDcqn7urwMLmibbkAFe0powKp78yLnvdzgU9cSk7jKNwKPTQ/132",
+    "nick_name": "秋霞^_^",
+    "mobile": "15623214452"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/x2LvGTacUKuwgCTxgDFubZnKylCIzJJJwECMglaibMCKGERQUHliba90xfpIH6MXm3uN7JbwrmTNWianopxQkz2Hw/132",
+    "nick_name": "柳",
+    "mobile": "15527502780"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELwK4jY5q4Ud7QX5gT2zMprs5rvSkTASjoXRgqhxxfveBltQJwIH5VaZd8XrUIqfWCFPyIFf0ODFA/132",
+    "nick_name": "陶陶",
+    "mobile": "15607123678"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKtjlLsAdNV7Nchhw3RhmO4HibpJwicg4pYAHzNSQOWfS9FwSvCubuoLAnSFIqSaYClx828grd3fffg/132",
+    "nick_name": "秋水鸣蛙",
+    "mobile": "18007151577"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/2q2jpbOxKz1lEqiaQ9pk8bspibxtdsNNlC4fQIm8qgfIVgD1nZl2D1ywfkA2Zv3kvsHBIPLWla8FD1QmN6wiaoPDw/132",
+    "nick_name": "妮子牛",
+    "mobile": "13545911408"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/IaCgQ7KiciacuJqHC8uY5siamroCxGgV1QnluLlRRD1eIicyInAXxnPcLiaItC6sUQdAibMJsFTyR4Mz2oJgcicXTic0lQ/132",
+    "nick_name": "💋.J",
+    "mobile": "15927300290"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/eFNylkVW9YxMIaaKvZN9t6Ul3RJwb2CzKXwYtvVwOMfosTcD2MVrmagibuozBcIUqKLgEXCwnnvC3cJ7duhL0uw/132",
+    "nick_name": "雍嬅",
+    "mobile": "13627244242"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJR5YboSMO0tJZHoqGLOXML981MBy1BVfxsNDicqp63yK74evHsniaHiciaTaw25bUzaJT3041icIPMekQ/132",
+    "nick_name": "sweet🍀",
+    "mobile": "18872204162"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIiazvyNSYVOp07pY4y4caOq03yOu3u6icrcPpAwgZD81U8VCpXuCIVrMNh5HJwicqJC65GGaaSlj1LA/132",
+    "nick_name": "恭小维",
+    "mobile": "18571667471"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLKz8zuu3kJlHxW3FLiaCmOvwhmNQ32rKPxSycC751WaXiazCs2gXIox79IZYNLSicRZsq9vvicV4cO8Q/132",
+    "nick_name": "Alice",
+    "mobile": "13207185112"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLibxc3gdRmc0zQ1rO3ZrbvUZ6a5uEaZFwrKKQDrUKNEadBic6gR7oUMuicQVicVMjpyGCLIjx7cFnUYw/132",
+    "nick_name": "蜻蜓",
+    "mobile": "18672754757"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/XgDtWeiaKW60F9HX2zw4vdwrT2PIDGqh9Gw506IG5FqmO7yzIVWThbW2685w5Ugm9ia1UP9sJnWkXlBfictgQeeUw/132",
+    "nick_name": "李纯",
+    "mobile": "17786140815"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/qXaXNviaJuS7iaU7R2212qQLoa1bibsbtVPFUD2dKrL32vdEpaWpiauqtE1nibtyhZ9tzsZNvP6B8T4NMWib6O0IB4yg/132",
+    "nick_name": "乌龟插错电",
+    "mobile": "13659867155"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLnh4U8IT9DzKMQ1CGpz8B8yl0DRd6qZPoRyTlFox5kHjdmwf7ia2qvBuDjbKD9PeelEicJ8F97n6rQ/132",
+    "nick_name": "韩明亮",
+    "mobile": "17600101192"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKF7FeGcRm3Xib6jZsQeHeoGajKBNWV7zRgUia4OjibiboZwC69icNafO3VpyZvTuZIPW48AuCGjQGdfjw/132",
+    "nick_name": "洛",
+    "mobile": "13687271261"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTL0TBp8d8hvaicx2agia6DcfInFA0nnHiceIIyUCj4NY2SnTUcaYneEpfKNGN4A7aOAqJHReBH44SaFw/132",
+    "nick_name": "陈钢",
+    "mobile": "13517269360"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIRnggTickOvKHibtxN6PYEeF7nG0zh3g4aEOd1YqrWMjPl7UibKoF324jxiay1EtGVINkx9wRuoxDELw/132",
+    "nick_name": "赵家伟",
+    "mobile": "18086000706"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIRnggTickOvKHibtxN6PYEeF7nG0zh3g4aEOd1YqrWMjPl7UibKoF324jxiay1EtGVINkx9wRuoxDELw/132",
+    "nick_name": "赵家伟",
+    "mobile": "18086000706"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/cGOHsoZe2iaTPaVbfNicZy8M43icdCz1WAhzJbibA9AdoH3Oy0ad8WkCc5z3Huo1jpgsiatAtpHLl6zt60T9j8H3Dyg/132",
+    "nick_name": "U",
+    "mobile": "18872242312"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/cGOHsoZe2iaTPaVbfNicZy8M43icdCz1WAhzJbibA9AdoH3Oy0ad8WkCc5z3Huo1jpgsiatAtpHLl6zt60T9j8H3Dyg/132",
+    "nick_name": "U",
+    "mobile": "18872242312"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoeg8qudqCemGxOeMqUFUHQdcYJBib9MxDBVWmxjAZIhMLMEBwCRdVDXv5cFZKW3SZAW312C7ATichA/132",
+    "nick_name": "Cheng",
+    "mobile": "17671666385"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJqM0lmbX99Pbkee0t7Fe21bEd4I31UHg8R5SjKsbVUy96hNLDsN0ctbsJ2EyWkRZ5R2paVt0LdLQ/132",
+    "nick_name": "从无到有Sunry",
+    "mobile": "13995666293"
+  },
+  {
+    "avatar_url": "https://wx.qlogo.cn/mmopen/vi_32/TWngtnAicPhmO0W8gk4Q5aMxv116ic1xMV1ibzOKGqyaSHMCGp6ciaS3EWwQgzKDARMps6GkOFXKiaiaNfWSWt6sfAPQ/132",
+    "nick_name": "微蓝",
+    "mobile": "17371063499"
+  }
+]
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var curNum = 0
 var curId = getQueryVariable('id')
 localStorage.setItem('curId', curId)
 
-var locaScreen = [20, 70, 120, 170, 220, 270, 320, 370, 420, 470, 520, 570, 620, 670, 720]; //弹幕位置
+var locaScreen = [20, 70, 120, 170, 220, 270, 320, 370, 420, 470, 520, 570, 620, 670]; //弹幕位置
 var timer;
 var timer1;
 init()
@@ -76,7 +76,7 @@ var olN = 0
 function createScreen (elem) {
   var _top = 0;
   var _left = $(window).width();
-  var lN = Math.floor(15 * Math.random());
+  var lN = Math.floor(14 * Math.random());
   if (olN == lN) {
     lN++;
     if (lN > locaScreen.length - 1) {
